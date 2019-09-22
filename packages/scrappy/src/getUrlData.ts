42dd2b5a8ce8295f6fac8c82c0603135a802f5cb
FelipeAcosta02 <jesusfacosta02@gmail.com>
2019-09-22 09:30:52 -0600

    CRA client

--- packages/scrappy/src/getUrlData.ts
+++ packages/scrappy/src/getUrlData.ts
@@ -1,9 +1,9 @@
 import cheerio from "cheerio";
 import request from "request";
-export function getUrlData(
+export default async function getUrlData(
   url: string,
   callback: (data?: CheerioStatic, error?: string) => void
-) {
+): Promise<void> {
   const _ = undefined;
   request(url, (err, __, html) => {
     if (err) callback(_, "Error getting url data");
