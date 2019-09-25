2f556d1dbe7b096752dc326389ef15ea47729de1
FelipeAcosta02 <jesusfacosta02@gmail.com>
2019-09-25 16:59:19 -0600

    Updates

---
+++ precache-manifest.76896b905fc31af16c087e6705aa11f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71aa4d2cf894c2202946fb463c20cd06",
+    "url": "/gilscrappers/index.html"
+  },
+  {
+    "revision": "a3bface226e3f7f0d244",
+    "url": "/gilscrappers/static/css/main.dbf6cf6c.chunk.css"
+  },
+  {
+    "revision": "5c119e66bb6c07f9a367",
+    "url": "/gilscrappers/static/js/2.9d2e3738.chunk.js"
+  },
+  {
+    "revision": "a3bface226e3f7f0d244",
+    "url": "/gilscrappers/static/js/main.cfdf115d.chunk.js"
+  },
+  {
+    "revision": "4d28e0cf9aced741df49",
+    "url": "/gilscrappers/static/js/runtime-main.5725a0f6.js"
+  }
+]);
